eaecf89e9b031a0ec8f2fd1ce89576ef1967b322
Naresh Tak <taknaresh515@gmail.com>
2023-05-10 17:56:44 +0530

    change some js

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ $(document).ready(function () {
         type: 'iframe',
         gallery: {
             enabled: true
-        },
-        src: $('<video controls>\
-                      <source src="'+ $(this).attr('href') + '" type="video/mp4">\
-                      Désolé, votre navigateur ne supporte pas les vidéos.\
-                    </video>')
+        }
     });
 })
