3ab00c36946d8c5760a41bf7b262cb0b27e8cd62
Naresh Tak <taknaresh515@gmail.com>
2023-05-11 11:51:20 +0530

    make video show

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
     var currentvidIndex = 0;
     var videos = $(".img-fluid");
+    const videoStr = 'https://www.youtube.com/embed/';
+    var videoSo = '';
 
     $(".img-fluid").click(function () {
-        var videoSo = $(this).attr("source");
-        currentvidIndex = videos.index(this);
-        $(".img-overlay").css("display", "flex");
-        $(".zoom-img").append("<video  source='" + videoSo + "'>");
-        $("body").addClass("overlay-open");
+        videoSo = $(this).data('id');
+        if (videoSo) {
+            console.log(videoStr + videoSo)
+            currentvidIndex = videos.index(this);
+            $(".img-overlay").css("display", "flex");
+            $(".zoom-img").append("<iframe src='" + videoStr + videoSo + "' ></iframe>");
+            $("body").addClass("overlay-open");
+        }
     });
 
     $(".close-btn").click(function () {
         $(".img-overlay").css("display", "none");
-        $(".zoom-img img").remove();
+        $(".zoom-img iframe").remove();
         $("body").removeClass("overlay-open");
     })
 
@@ -25,29 +30,21 @@ $(document).ready(function () {
     });
 
     $(".prev-btn").click(function () {
-        currentImgIndex--;
-        if (currentImgIndex < 0) {
-            currentImgIndex = imgs.length - 1;
+        currentvidIndex--;
+        if (currentvidIndex < 0) {
+            currentvidIndex = videos.length - 1;
         }
-        var imgSrc = $(imgs[currentImgIndex]).attr("src");
-        $(".zoom-img img").css("opacity", "0");
-        setTimeout(function () {
-            $(".zoom-img img").attr("src", imgSrc);
-            $(".zoom-img img").css("opacity", "1");
-        }, 500);
+        videoSo = $(videos[currentvidIndex]).data('id');
+        $(".zoom-img iframe").attr("src", videoStr + videoSo);
     });
 
     $(".next-btn").click(function () {
-        currentImgIndex++;
-        if (currentImgIndex >= imgs.length) {
-            currentImgIndex = 0;
+        currentvidIndex++;
+        if (currentvidIndex >= videos.length) {
+            currentvidIndex = 0;
         }
-        var imgSrc = $(imgs[currentImgIndex]).attr("src");
-        $(".zoom-img img").css("opacity", "0");
-        setTimeout(function () {
-            $(".zoom-img img").attr("src", imgSrc);
-            $(".zoom-img img").css("opacity", "1");
-        }, 500);
+        videoSo = $(videos[currentvidIndex]).data('id');
+        $(".zoom-img iframe").attr("src", videoStr + videoSo);
     });
 
 });
