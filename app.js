32c5ea4bc707e3731cbee64fc570d97a27ba8505
Naresh Tak <taknaresh515@gmail.com>
2023-05-11 10:45:02 +0530

    change layout

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 $(document).ready(function () {
-    $('.video-gallery').magnificPopup({
-        delegate: 'a',
-        type: 'iframe',
-        gallery: {
-            enabled: true
-        }
+    var currentvidIndex = 0;
+    var videos = $(".img-fluid");
+
+    $(".img-fluid").click(function () {
+        var videoSo = $(this).attr("source");
+        currentvidIndex = videos.index(this);
+        $(".img-overlay").css("display", "flex");
+        $(".zoom-img").append("<video  source='" + videoSo + "'>");
+        $("body").addClass("overlay-open");
     });
-})
+
+    $(".close-btn").click(function () {
+        $(".img-overlay").css("display", "none");
+        $(".zoom-img img").remove();
+        $("body").removeClass("overlay-open");
+    })
+
+    $(".zoom-img").slick({
+        arrows: true,
+        prevArrow: $(".prev-btn"),
+        nextArrow: $(".next-btn"),
+        fade: true,
+        speed: 500,
+    });
+
+    $(".prev-btn").click(function () {
+        currentImgIndex--;
+        if (currentImgIndex < 0) {
+            currentImgIndex = imgs.length - 1;
+        }
+        var imgSrc = $(imgs[currentImgIndex]).attr("src");
+        $(".zoom-img img").css("opacity", "0");
+        setTimeout(function () {
+            $(".zoom-img img").attr("src", imgSrc);
+            $(".zoom-img img").css("opacity", "1");
+        }, 500);
+    });
+
+    $(".next-btn").click(function () {
+        currentImgIndex++;
+        if (currentImgIndex >= imgs.length) {
+            currentImgIndex = 0;
+        }
+        var imgSrc = $(imgs[currentImgIndex]).attr("src");
+        $(".zoom-img img").css("opacity", "0");
+        setTimeout(function () {
+            $(".zoom-img img").attr("src", imgSrc);
+            $(".zoom-img img").css("opacity", "1");
+        }, 500);
+    });
+
+});
